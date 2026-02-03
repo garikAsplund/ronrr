023533bc5304ffac3ab7a747dfc8cdf06ff71d90
Garik Asplund <garik.asplund@gmail.com>
2026-02-02 23:34:02 -0800

    icon, analytics, bed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import NavBar from '$lib/NavBar.svelte';
 	import Footer from '$lib/Footer.svelte';
 
