dcbe87616b219e45d486f1f1817f01f874f4310e
Garik Asplund <garik.asplund@gmail.com>
2026-02-09 07:23:54 -0800

    removed sober bowl poster

--- src/routes/meetings-activities/+page.svelte
+++ src/routes/meetings-activities/+page.svelte
@@ -10,11 +10,11 @@
 
 <section class="container mx-auto px-6 py-12">
 	<div class="flex flex-col space-y-6 md:flex-row md:space-y-0 md:space-x-6">
-		<enhanced:img
+		<!-- <enhanced:img
 			src={soberBowlUrl}
 			alt="Sober social event"
 			class="w-full rounded-lg object-cover shadow-lg md:w-1/2"
-		/>
+		/> -->
 		<enhanced:img
 			src={ballUrl}
 			alt="Recovery bowling night"
